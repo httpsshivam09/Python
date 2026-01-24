6093ca0e62f760de84e416c366132cdd7c5ccc89
httpsshivam09 <shivampatel790684@gmail.com>
2026-01-24 08:12:41 +0530

    Updated Python File

---
+++ Day01.py
@@ -0,0 +1,28 @@
+print("Hello World!")         
+name = "Shivam Patel "
+Dob = "20 July 2005"
+
+print (name.upper() + Dob)       
+
+
+
+num1 = 10.58562
+num2 = 54710.55
+
+sum = num1 + num2
+
+print(sum)
+
+print(round(num1, 1))
+
+
+
+
+
+
+
+
+
+
+
+
